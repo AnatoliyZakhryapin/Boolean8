e0aaaa9c4dc83c8f18d1154f96aea0975b5759ca
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2024-04-16 11:31:11 +0200

    Add project files.

---
+++ Boolean8/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Boolean8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
